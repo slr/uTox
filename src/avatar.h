9a2453809cee59d70aa5956a31bf70f5ddc3b09b
Gregory Mullen (GrayHatter) <greg@grayhatter.com>
2016-03-20 13:18:23 -0700

    double avatar size
    I'm just going to double the avatar size here because 64 is stupid low.
    1. friends should be using a real client that'll store the avatar until changed (I'm looking at you qtox)
    2. 128k is still ~0.1M so still tiny for an image even if the image is just supposed to be an icon
    3. I know this breaks tacos, don't care that much AFAIK, no other client enforce that size

--- src/avatar.h
+++ src/avatar.h
@@ -2,7 +2,7 @@
 #define AVATAR_DIRECTORY "avatars"
 
 //TODO: remove
-#define UTOX_AVATAR_MAX_DATA_LENGTH (64 * 1024) //NOTE: increasing this above 64k might cause issues.
+#define UTOX_AVATAR_MAX_DATA_LENGTH (128 * 1024) //NOTE: increasing this above 64k might cause issues.
 #define UTOX_AVATAR_FORMAT_NONE 0
 #define UTOX_AVATAR_FORMAT_PNG 1
 
